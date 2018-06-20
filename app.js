536c6938e2c3510514d048e4b11280527288ebed
Christian Benitez <cbenitez@sd.com.py>
2018-06-20 18:20:18 -0400

    commit

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const axios = require('axios');
-const opts = require('./config/yargs').argv; 
+const argv = require('./config/yargs').argv; 
 
-let urlencode = decodeURIComponent(  )
+let urlencode = decodeURIComponent( argv.direccion );
 
-https://maps.googleapis.com/maps/api/geocode/json?address=Asuncion,Paraguay&key=AIzaSyALC-GhyIM9eJKalYxKX_4fF_SMgBsQvw8
+console.log( urlencode + "\n" );
+
+axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${ urlencode }&key=AIzaSyALC-GhyIM9eJKalYxKX_4fF_SMgBsQvw8`)
+    .then( resp => {
+        let data = resp.data.results[0];
+        console.log( 'address', data.formatted_address );
+        console.log( 'lat', data.geometry.location.lat );
+        console.log( 'lng', data.geometry.location.lng );
+        /*
+        console.log( JSON.stringify( resp.data, undefined, 2 ) );
+        */
+    })
+    .catch( e => console.log( 'Error!' , e ) );
