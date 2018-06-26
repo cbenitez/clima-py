b737f96fada66adf8712be75de2d851cbe29b5fe
Christian Benitez <cbenitez@sd.com.py>
2018-06-26 17:16:00 -0400

    clima terminado

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 const lugar = require('./lugar/lugar');
+const clima = require('./clima/clima');
 const argv = require('./config/yargs').argv; 
 
-lugar.getLugarLatLng( argv.direccion )
-    .then(resp => console.log(resp) )
-    .catch(e => console.log(e)) ;
+//lugar.getLugarLatLng( argv.direccion )
+//    .then(resp => console.log(resp) )
+//    .catch(e => console.log(e)) ;
 
+//clima.getClima(-25.2637399, -57.57592599999999)
+//        .then( temp => console.log(temp) )
+//        .catch(e=>console.log(e));
 
+let getInfo = async( direccion ) => {
+
+	try{
+		let coors = await lugar.getLugarLatLng( direccion );
+		let temp = await clima.getClima( coors.lat, coors.lng );
+	
+		return `El clima en ${ coors.direccion } es de ${ temp.temp } grados`;
+	}catch( e ){
+		return `No se pudo determinar el clima en ${ direccion }`;
+	}
+
+}
+
+getInfo( argv.direccion )
+	.then( mensaje => console.log( mensaje ) )
+	.catch( e => console.log( e ) );
--- clima/clima.js
+++ clima/clima.js
@@ -1,1 +1,24 @@
-'http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&units=metric&appid=4a99ba9a7b241b23259c762411bb7e09'
+const axios = require('axios');
+
+
+const getClima = async (lat,lng) => {
+
+    let resp = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=metric&appid=4a99ba9a7b241b23259c762411bb7e09`);
+    
+    if ( resp.cod === 400 ) {
+        throw new Error(`No hay resultados para: ${lat} y ${lng}`);
+    }
+
+    let main = resp.data.main;
+
+    return {
+        temp: main.temp,
+        temp_min: main.temp_min,
+        temp_max: main.temp_max
+    }
+}
+
+
+module.exports = {
+    getClima
+}
